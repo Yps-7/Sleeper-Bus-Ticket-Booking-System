4f9df9782e2f51f7f8ef6cfc74d71e80a635e5ae
Yuvi_7 <115544577+Yps-7@users.noreply.github.com>
2026-01-25 14:48:18 +0530

    Refine comments and correct typo in data splitting
    Updated comments for clarity and fixed a typo in the split data section.

--- Proposed_model.py
+++ Proposed_model.py
@@ -1,4 +1,5 @@
 # HERE WE GO for making Bus Booking Confirmation prediction model(prposed model) BY USING RANDOM FOREST CLASSIFIER
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -14,18 +15,16 @@ data = pd.DataFrame({
     'Confirmed' : np.random.choice([0,1], size = 10000, p =[0.3,0.7])
 })
 #print(data.head(17)) # agar banaya hua data dekhna ho to
-
 # Prepare data
 data = pd.get_dummies(data, columns=['deposit'])
 X = data.drop('confirmed', axis=1)
 y = data['confirmed']
 
-# Split and train **RANDOM FOREST**
+# Split data into train adn test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-# Predict % confirmation for NEW booking
 new_booking = pd.DataFrame({
     'lead_time': [20], 
     'past_cancels': [1], 
@@ -34,4 +33,4 @@ new_booking = pd.DataFrame({
 })
 
 prob_confirm = model.predict_proba(new_booking)[0][1] * 100
-print(f"Confirmation probability: {prob_confirm:.1f}%")  # e.g., 82.5%
+print(f"Confirmation probability: {prob_confirm:.1f}%") 
